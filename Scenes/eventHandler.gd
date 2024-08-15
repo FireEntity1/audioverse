4d16646c3badca74958b7504c4c760db896294c6
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-08-15 00:06:47 -0600

    Fog colour syncing

--- Scenes/eventHandler.gd
+++ Scenes/eventHandler.gd
@@ -3,6 +3,9 @@ extends Node3D
 var running = false
 var frames = 10
 
+var vocal
+var bass
+
 # BPM 138.6
 
 func _ready():
@@ -10,8 +13,11 @@ func _ready():
 
 
 func _process(delta):
-	print(AudioServer.get_bus_volume_db(0))
-
+	var spectrum = AudioServer.get_bus_effect_instance(0,0)
+	# print((spectrum.get_magnitude_for_frequency_range(0,100).x + spectrum.get_magnitude_for_frequency_range(0,100).y)/2)
+	vocal = ((spectrum.get_magnitude_for_frequency_range(300,3000).x + spectrum.get_magnitude_for_frequency_range(300,5000).y)/2)/1.2
+	bass = ((spectrum.get_magnitude_for_frequency_range(0,300).x + spectrum.get_magnitude_for_frequency_range(0,300).y)/2)/1.2
+	$player/neck/camera.environment.fog_light_color = Color(lerp($player/neck/camera.environment.fog_light_color.r,vocal,1),0,lerp($player/neck/camera.environment.fog_light_color.b,bass/2,1))
 func _physics_process(delta):
 	if running:
 		frames -= 1
