832de17f287c64ba1d590b96cf2870849c83fd3f
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-08-15 13:13:41 -0600

    More lasers + no bpm dependancy

---
+++ Scenes/vr.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+
+var perform_runtime_config = false
+
+
+
+
+#func _ready():
+	#var interface = ARVRServer.find_interface("OVRMobile")
+	#if interface:
+		#ovr_init_config.set_render_target_size_multiplier(1)
+#
+		#if interface.initialize():
+			#get_viewport().arvr = true
+
+
+func _process(_delta):
+	pass
