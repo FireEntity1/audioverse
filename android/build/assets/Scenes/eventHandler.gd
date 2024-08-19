8ef72b28bcd071067cea40b8a08ded22d3a442fb
FireEntity1 <134467282+FireEntity1@users.noreply.github.com>
2024-08-19 13:36:58 -0600

    Attempt at song loading + Intensity controls

--- android/build/assets/Scenes/eventHandler.gd
+++ android/build/assets/Scenes/eventHandler.gd
@@ -16,13 +16,10 @@ var interface : MobileVRInterface
 func _ready() -> void:
 	interface = XRServer.find_interface("OVRMobile")
 	get_viewport().use_xr = true
-	if interface and interface.is_initialized():
-		print("yipee")
-		
+	# load_ogg("/Music/song.ogg")
 
 
 func _process(delta):
-	
 	var spectrum = AudioServer.get_bus_effect_instance(0,0)
 	vocal = ((spectrum.get_magnitude_for_frequency_range(400,3000).x + spectrum.get_magnitude_for_frequency_range(400,3000).y)/2)/1.2
 	bass = ((spectrum.get_magnitude_for_frequency_range(120,300).x + spectrum.get_magnitude_for_frequency_range(120,300).y)/2)/1.2
@@ -45,3 +42,27 @@ func _physics_process(delta):
 
 func _on_bpm_timer_timeout():
 	running = true
+
+func load_mp3(path):
+	var file = FileAccess.open(path, FileAccess.READ)
+	var sound = AudioStreamMP3.new()
+	sound.data = file.get_buffer(file.get_length())
+	$player2.stream = sound
+	$player2.play()
+	
+func load_ogg(path):
+	$player2.stream = AudioStreamOggVorbis.load_from_file(path)
+	$player2.play()
+
+func _on_file_choose_file_selected(path):
+	if path.ends_with(".mp3"):
+		load_mp3(path)
+	elif path.ends_with(".ogg"):
+		load_ogg(path)
+
+
+func _on_xr_controller_3d_button_released(name):
+	if name == "ax_button":
+		intensity -= 0.1
+	if name == "by_button":
+		intensity += 0.1
